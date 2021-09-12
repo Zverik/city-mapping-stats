2fd69f321b01a684724eef4adcefed1fcdd3ef56
Ilya Zverev <zverik@textual.ru>
2021-09-12 11:40:42 +0300

    Updates, better handling of split and joined ways

--- counter/adiff_to_csv.py
+++ counter/adiff_to_csv.py
@@ -10,18 +10,29 @@ from shapely.strtree import STRtree
 
 
 COLUMNS = [
+    # UTC timestamp for the change
     ('ts', 'timestamp with time zone not null'),
+    # One of create, delete, modify,
+    # split (created from splitting), join (deleted for joining)
     ('action', 'text not null'),
+    # For a tag: create, delete, modify (for a value)
     ('obj_action', 'text not null'),
+    # Tag kind, e.g. crossing, maxspeed
     ('kind', 'text not null'),
+    # System data from an object
     ('changeset', 'integer not null'),
     ('uid', 'integer not null'),
     ('username', 'text not null'),
     ('osm_id', 'text not null'),
     ('version', 'integer not null'),
+    # For splitting and joining, osm_id of an ancestor way
+    ('prev_id', 'text'),
+    # When filtering by regions, a region name
     ('region', 'text'),
+    # Location of a node or a centroid
     ('lat', 'double precision not null'),
     ('lon', 'double precision not null'),
+    # For ways, length in meters
     ('length', 'integer'),
 ]
 
@@ -32,6 +43,9 @@ class Regions:
         self.region_map = {}
         if fileobj:
             self.load(fileobj)
+
+    def is_empty(self):
+        return self.tree is None or len(self.region_map) == 0
 
     def load(self, fileobj):
         regions = []
@@ -56,13 +70,17 @@ def get_float_attr(attr, obj, backup=None):
     return float(backup.get(attr))
 
 
+def get_osm_id(obj):
+    return f'{obj.tag}/{obj.get("id")}'
+
+
 def init_data_from_object(obj, backup=None):
     result = {
         'ts': obj.get('timestamp').replace('T', ' ').replace('Z', '+00'),
         'changeset': obj.get('changeset'),
         'uid': obj.get('uid'),
         'username': obj.get('user'),
-        'osm_id': f'{obj.tag}/{obj.get("id")}',
+        'osm_id': get_osm_id(obj),
         'version': obj.get('version'),
     }
     if obj.tag == 'node':
@@ -183,63 +201,91 @@ def find_way_in_another_modified(way, adiff, is_created: bool):
     return None
 
 
+def write_header(output, table=None):
+    col_names = ','.join(c[0] for c in COLUMNS)
+    if not table:
+        output.write(col_names + '\n')
+    else:
+        output.write(f"create table if not exists {table} (\n")
+        for c in COLUMNS:
+            comma = '' if c == COLUMNS[-1] else ','
+            output.write(f"    {c[0]} {c[1]}{comma}\n")
+        output.write(");\n")
+        output.write(f"copy {table} ({col_names}) from stdin (format csv);\n")
+
+
+def process_single_action(action, adiff, regions=None):
+    """
+    Processes a single action in an augmented diff.
+    Returns a list of rows to print.
+    """
+    atype = action.get('type')
+    obj = action[0] if atype == 'create' else action.find('new')[0]
+    if obj.tag == 'relation':
+        # We do not process relations.
+        return
+    old = None if atype == 'create' else action.find('old')[0]
+    data = init_data_from_object(obj, old)
+    if regions and not regions.is_empty():
+        data['region'] = regions.find(data['lon'], data['lat'])
+        if not data['region']:
+            return
+    # Note that for deleted objects "obj" has all its data,
+    # and "old" has just some of the header values.
+    ancestor = None
+    if obj.tag == 'way':
+        if atype == 'create':
+            ancestor = find_way_in_another_modified(obj, adiff, True)
+            if ancestor is not None:
+                # Some way was split into this (and possibly others).
+                sys.stderr.write(f'Found an ancestor {ancestor.get("id")} for '
+                                 f'the created way {obj.get("id")}!\n')
+                atype = 'split'
+                data['prev_id'] = get_osm_id(ancestor)
+                old = ancestor  # just for comparing tags
+        elif atype == 'delete':
+            ancestor = find_way_in_another_modified(old, adiff, False)
+            if ancestor is not None:
+                # This way (and possibly others) were merged into the ancestor.
+                sys.stderr.write(f'Found an ancestor {ancestor.get("id")} for '
+                                 f'the deleted way {old.get("id")}!\n')
+                atype = 'join'
+                data['prev_id'] = get_osm_id(ancestor)
+                obj = ancestor  # just for comparing tags
+    data['obj_action'] = atype
+    # Find tagging differences and write them out.
+    kinds = get_kinds(obj, old)
+    for k in kinds:
+        data['action'] = k[1]
+        data['kind'] = k[0]
+        yield data
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Extracts road changes from an augmented diff file.')
     parser.add_argument('adiff', type=argparse.FileType('rb'),
                         help='Augmented diff file')
     parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout,
-                        help='Output CSV file')
+                        help='Output CSV or SQL file')
     parser.add_argument('-r', '--regions', type=argparse.FileType('r'),
                         help='CSV file with names and wkb geometry for regions to filter')
     parser.add_argument('-t', '--table',
                         help='Instead of CSV, print SQL for importing into this psql table')
     options = parser.parse_args()
 
+    # Read regions and the augmented diff.
     regions = Regions(options.regions)
     adiff = etree.parse(options.adiff).getroot()
+
+    # Prepare writer and write the header.
     writer = csv.DictWriter(options.output, [c[0] for c in COLUMNS])
-    if not options.table:
-        writer.writeheader()
-    else:
-        options.output.write(f"create table if not exists {options.table} (\n")
-        for c in COLUMNS:
-            comma = '' if c == COLUMNS[-1] else ','
-            options.output.write(f"    {c[0]} {c[1]}{comma}\n")
-        col_names = ','.join(c[0] for c in COLUMNS)
-        options.output.write(f"copy {options.table} ({col_names}) from stdin (format csv);\n")
+    wrote_header = False
+
+    # Iterate over every action (each of which has just one object).
     for action in adiff.findall('action'):
-        atype = action.get('type')
-        obj = action[0] if atype == 'create' else action.find('new')[0]
-        if obj.tag == 'relation':
-            continue
-        old = None if atype == 'create' else action.find('old')[0]
-        data = init_data_from_object(obj, old)
-        # Note that for deleted objects "obj" has all its data,
-        # and "old" has just some of the header values.
-        ancestor = None
-        if obj.tag == 'way':
-            if atype == 'create':
-                ancestor = find_way_in_another_modified(obj, adiff, True)
-                if ancestor is not None:
-                    sys.stderr.write(f'Found ancestor {ancestor.get("id")} for '
-                                     f'created way {obj.get("id")}!\n')
-                    old = ancestor
-            elif atype == 'delete':
-                ancestor = find_way_in_another_modified(old, adiff, False)
-                if ancestor is not None:
-                    sys.stderr.write(f'Found ancestor {ancestor.get("id")} for '
-                                     f'deleted way {old.get("id")}!\n')
-                    obj = ancestor
-        # TODO: so what do we do with this ancestor way?
-        # We need to fix lengths in data I guess
-        # Or register deletions for the old objects.
-        data['obj_action'] = atype
-        data['region'] = regions.find(data['lon'], data['lat'])
-        if options.regions and not data['region']:
-            continue
-        kinds = get_kinds(obj, old)
-        for k in kinds:
-            data['action'] = k[1]
-            data['kind'] = k[0]
-            writer.writerow(data)
+        for row in process_single_action(action, adiff, regions):
+            if not wrote_header:
+                write_header(options.output, options.table)
+                wrote_header = True
+            writer.writerow(row)
